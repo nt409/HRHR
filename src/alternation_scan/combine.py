0e7f9129a0c797f9dbf400f8e3e5b1b5cf9f4b54
nt409 <nt409@cam.ac.uk>
2022-02-07 15:29:52 +0000

    combine

---
+++ src/alternation_scan/combine.py
@@ -0,0 +1,7 @@
+from alternation_scan.utils import combine_alt_scan_csvs
+
+if __name__ == "__main__":
+    n_doses = 51
+    n_its = 100
+
+    combine_alt_scan_csvs(n_doses, n_its)
--- src/alternation_scan/utils.py
+++ src/alternation_scan/utils.py
@@ -51,31 +51,17 @@ def random_alt_scan_params(index):
     return primary_inoc, fcide_parms
 
 
-def combine(n_doses, n_its):
+def combine_alt_scan_csvs(n_doses, n_its):
     df = pd.DataFrame()
 
-    for alt_strat, index in itertools.product(["alt_12", "alt_21"], list(range(n_its))):
+    for index in range(n_its):
 
-        filename = f"./alternation_scan/outputs/single/out_rand_{n_doses}_{alt_strat}_{index}.csv"
+        filename = f"./alternation_scan/outputs/single/scan_rand_output_{n_doses}_{index}.csv"
 
         new_df = pd.read_csv(filename)
-        new_df["alt_strat"] = alt_strat
 
         df = pd.concat([df, new_df])
 
-    # print(df.head(-10))
-    # print(df.describe())
-
-    print("WORKED RUNS; MIX AT LEAST AS GOOD")
-    print(df[df["asex_alt"] <= df["asex_mix"]])
-    print(df[df["sex_alt"] <= df["sex_mix"]])
-
-    print("FAILED RUNS; ALT OUTPERFORMS MIX")
-    print(df[df["asex_alt"] > df["asex_mix"]])
-    print(df[df["sex_alt"] > df["sex_mix"]])
-
-    print("NB 100 iterations but over alt_21 and alt_12")
-
-    filename = f"./alternation_scan/outputs/combined/out_{n_its}_{n_doses}_{alt_strat}.csv"
+    filename = f"./alternation_scan/outputs/combined/out_{n_its}_{n_doses}.csv"
     print(f"saving df to: {filename}")
     df.to_csv(filename, index=False)
